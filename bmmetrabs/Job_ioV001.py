4ac147c312c0522dc21d55c3c8a2f7eb0ca80e97
me@nowhere.net <me@nowhere.net>
2023-04-13 01:55:20 +0200

    Add Job_IO suite

---
+++ bmmetrabs/Job_ioV001.py
@@ -0,0 +1,153 @@
+#!/usr/bin/env python3
+import resource
+import gc
+import os
+import json
+import sys
+from os.path import exists
+
+version = 3
+
+# choose skeleton
+skeleton = ''
+#skeleton = 'smpl+head_30'
+# skeleton = 'coco_19'
+# skeleton = 'h36m_17'
+# skeleton = 'h36m_25'
+# skeleton = 'mpi_inf_3dhp_17'
+#skeleton = 'mpi_inf_3dhp_28'
+
+
+# set other parameters here
+datapath = 'Cris26'
+# set appenix
+respath = datapath + ''
+frame_start = 730
+frame_end = 1030  # 1160
+max_detections=1
+jobfilename = 'e2fjob'+datapath+''+'.json'
+create_json = 1  # enum 0 dont 1 skip existing 2 overwrite
+fov_degrees = 30
+qual = 0  # v: v<1 lowest v<20 medium v<50 good else above best
+cpu_count = 8
+viz = 3
+skip = 1
+#adust pathes
+inpath = 'input/' + datapath
+outpath = 'output/res' + respath
+
+inpattern = inpath +'/Image{0:04d}.png'
+#inpattern = inpath +'/image{0:04d}.png'
+
+
+
+# create default file
+if (0):
+	datapath = 'default'
+	# set appenix
+	respath = datapath
+	inpath = 'input/' + datapath
+	outpath = 'output/res' + respath
+	frame_start = 1
+	frame_end = 10
+	jobfilename = 'e2fjob'+datapath+'.json'
+	create_json = 2  # enum 0 dont 1 skip existing 2 overwrite
+	fov_degrees = 90
+	qual = 15  # enum 0 lowest 19 meduim 49 good above 100 best
+	cpu_count = 1
+	viz  = 1
+	skip = 1
+
+
+        
+
+class Job_io:
+    @classmethod
+    def __init__(self,name = jobfilename):
+        print('class __init__ Jobfile', name)
+        self._myname = name
+
+    @classmethod
+    def myname(self):
+    	#print('func myname smn',self._myname)
+    	return self._myname
+
+    @classmethod
+    def make_job_input(self):
+        with open(self.myname(), 'w') as ji:
+            jdata = {}
+            jdata['version'] = version
+            jdata['max_detections'] = max_detections
+            jdata['fov_degrees'] = fov_degrees
+            jdata['skeleton'] = skeleton
+            jdata['start'] = frame_start
+            jdata['end'] = frame_end
+            jdata['skip'] = skip
+            jdata['viz'] = viz
+            jdata['qual'] = qual
+            jdata['cpu_count'] = cpu_count
+            jdata['create_json'] = create_json
+            jdata['inpattern'] = inpattern
+            jdata['outpath'] = outpath
+            jdata['outputpatternjson'] = '/Posedata{0:04d}.json'
+            jdata['outputpatternviz'] = '/viz/Image{0:04d}.png'
+            jdata['outputpatternviz1'] = '/viz1/Image{0:04d}.png'
+            jdata['outputpatternviz2'] = '/viz2/Image{0:04d}.png'
+            json.dump(jdata, ji, ensure_ascii=False, indent=4)
+            ji.close()
+
+    @classmethod           	
+    def read_job_input(self):
+        try: 
+            with open(self.myname()) as json_file:
+                jdata = json.load(json_file)
+                return jdata
+        except:
+            print('read_job_input()->except',self.myname())
+            return None
+
+    
+def main():
+
+    # total arguments
+    #n = len(sys.argv)
+    #print("Total arguments passed:", n)
+    # Arguments passed
+    #print("\nName of Python script:", sys.argv[0])
+    #print("\nArguments passed:", end = " ")
+    #for i in range(1, n):
+    	#print(sys.argv[i], end = " ")
+
+    jobio = Job_io()
+    #jobio = Job_io('something_else.json')
+    
+    res = jobio.read_job_input()
+    if (res == None):
+        jobio.make_job_input()
+        print(jobio.myname(),'created' )
+        res = jobio.read_job_input()
+        print ('The job is',res)
+
+    else :
+        print ('found job is',res)
+        answ = input('Replace? [y/n] ')
+        if len(answ)>0:
+            if answ[0] ==  'y':
+                jobio.make_job_input()
+                print('Replaced->',jobio.myname())
+                res = jobio.read_job_input()
+                print ('New job is',res)
+            else:
+                if answ[0] ==  'n':
+                    print('no changes')
+                else:
+                    print(answ)
+                    print('so what?')
+        	
+        
+
+
+if __name__ == '__main__':
+    main()
+
+
